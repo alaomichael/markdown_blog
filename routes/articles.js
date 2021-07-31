c07772df68e6670bde7f92bacea0d831640b8c44
alaomichael <contactleomax@gmail.com>
2021-07-31 23:45:47 +0100

    updated the views files.

--- routes/articles.js
+++ routes/articles.js
@@ -3,17 +3,7 @@ const Article = require('./../models/article')
 const router = express.Router();
 
 router.get("/new", (req, res) => {
-  // res.render('articles/new', { article: new Article()})
-  const article = [{
-    title: "New article",
-    description:"This is the article description",
-    markdown:"markup"
-  },{
-    title: "New article 2",
-    description:"This is the article description 2",
-    markdown:"markup 2"
-  }]
-  res.render('articles/new', { article: article})
+  res.render('articles/new', { article: new Article()})
 });
 
 router.get("/edit/:id", async (req, res) => {
@@ -28,6 +18,17 @@ router.get('/:slug', async (req,res) => {
 })
 
 router.post("/", async (req, res,next) => {
+//  const article = [{
+//     title: "New article",
+//     description:"This is the article description",
+//     markdown:"markup"
+//   },{
+//     title: "New article 2",
+//     description:"This is the article description 2",
+//     markdown:"markup 2"
+//   }]
+//   res.render('articles/new', { article: article})
+
  req.article = new Article()
  next()
 }, saveArticleAndRedirect('new'));
--- server.js
+++ server.js
@@ -8,11 +8,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/markdownblog',{ useNewUrlParser: true,
 useUnifiedTopology: true, useCreateIndex: true })
 
-const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
-
-
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
@@ -28,7 +25,9 @@ app.use('/articles', articleRouter)
 
 // Not Found Page
 app.get("*", (req, res) => {
-  res.render('articles/notfound', { article: "Not Found"})
+    res.render('articles/notfound', { article: "Not Found"})
 });
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port} 🔥`);
